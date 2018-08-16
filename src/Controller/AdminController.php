907b7a7cdddc60ac119b6187214fd5366ce88347
adriftfromhome <sschermer@protonmail.com>
2018-08-16 15:09:48 +0200

    now each userrole change with notify the user of it via email

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -92,14 +92,21 @@ class AdminController extends Controller {
             ->getManager()
             ->getRepository(User::class);
         $users = $userRepository->findAll();
+
         $roleRepository = $this->getDoctrine()->getManager()->getRepository(Role::class);
         $admin = $roleRepository->find(1);
+
         if($user->getRoles() != $admin) {
 
             $user->setRoles($admin);
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
+
+            $email = $user->getEmail();
+            $reason = "Email/Account/admin.html.twig";
+            $this->sendMail($reason, $email);
+
             return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => true]));
         }        
         
@@ -112,14 +119,22 @@ class AdminController extends Controller {
             ->getManager()
             ->getRepository(User::class);
         $users = $userRepository->findAll();
+
         $roleRepository = $this->getDoctrine()->getManager()->getRepository(Role::class);
         $normal = $roleRepository->find(2);
+
         if ($user->getRoles() != $normal) {
 
             $user->setRoles($normal);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
+
+            $email = $user->getEmail();
+            $reason = "Email/Account/noAdmin.html.twig";
+            $this->sendMail($reason, $email);
+
             return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => true]));
         }
     }
@@ -130,6 +145,7 @@ class AdminController extends Controller {
             ->getManager()
             ->getRepository(User::class);
         $users = $userRepository->findAll();
+
         $roleRepository = $this->getDoctrine()->getManager()->getRepository(Role::class);
         $inactive = $roleRepository->find(3);
         $admin = $roleRepository->find(1);
@@ -137,14 +153,43 @@ class AdminController extends Controller {
         if ($user->getRoles() != $admin && $user->getRoles() != $inactive ) {
 
             $user->setRoles($inactive);
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
+
+            $reason = "Email/Account/inactive.html.twig";
+            $email = $user->getEmail();
+            $this->sendMail($reason, $email);
             return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => true]));
         } 
         else {
             return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => "unable"]));
         }
+    }
+
+
+    public function sendMail(string $reason, string $email) {
+        
+        $transport = new \Swift_SmtpTransport("localhost:1025");
+        $mailer = new \Swift_Mailer($transport);
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('support@inh.com')
+            ->setTo($email)
+            ->setBody(
+                $this->renderView(
+                    $reason
+                ),
+                'text/html'
+            )
+        ->addPart(
+            $this->renderView(
+                $reason
+            ),
+            'text/plain'
+        );
+
+        $mailer->send($message);
     }
 }
 
