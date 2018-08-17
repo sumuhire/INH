94675b8039c8286aaa5dfbfaaa7b276cb9814201
thytus <xdevelter@gmail.com>
2018-08-17 15:36:12 +0200

    created profile templates

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -7,26 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class QuestionController extends Controller
 {
-    public function question(Request $request){
-    //     $manager = $this->getDoctrine()->getManager();
-    //     $question= new Question();
-    //     $questionForm = $this->createForm(QuestionFormType::class, $question, ['standalone' => true]);
-    //     $questionForm->handleRequest($request);
+    public function question(){
         
-    //     if ($questionForm->isSubmitted() && $questionForm->isValid()) {
-            
-    //         $manager->persist($question);
-    //         $manager->flush();
-            
-    //     }
-        
-    //     return $this->render(
-    //         'Default/question.html.twig',
-    //         [
-    //             'questionForm' => $questionForm->createView()
-    //         ]
-    //     );
-    
-    // }
-    
+    }
 }
