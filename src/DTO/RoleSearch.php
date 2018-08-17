97f3423234214332c3833caec774a29a400d7770
adriftfromhome <sschermer@protonmail.com>
2018-08-17 12:28:08 +0200

    changed userList to notify the admin on the role change behaviour

--- src/DTO/RoleSearch.php
+++ src/DTO/RoleSearch.php
@@ -1,19 +1,17 @@
 <?php
 namespace App\DTO;
 
-class UserSearch
+class RoleSearch
 {
 
     public $search;
-
-    public $role;
     /**
      * Get the value of project
      */
 
     /**
      * Get the value of search
-     */ 
+     */
     public function getSearch()
     {
         return $this->search;
@@ -22,28 +20,10 @@ class UserSearch
      * Set the value of search
      *
      * @return  self
-     */ 
+     */
     public function setSearch($search)
     {
         $this->search = $search;
-        return $this;
-    }
-
-    /**
-     * Get the value of project
-     */
-    public function getRole()
-    {
-        return $this->role;
-    }
-    /**
-     * Set the value of project
-     *
-     * @return  self
-     */
-    public function setRole($role)
-    {
-        $this->role = $role;
         return $this;
     }
 }
