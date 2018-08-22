f83ee2a4786c8a6dbf34eb1ad96783b16f130357
thytus <xdevelter@gmail.com>
2018-08-22 14:39:32 +0200

    template admin 3

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,103 +1,91 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
 use App\Form\InviteFormType;
 use App\Entity\Invite;
 use App\Entity\User;
 use App\Entity\Role;
-use App\Entity\Department;
 use App\DTO\UserSearch;
 use App\Form\UserSearchFormType;
-
+use App\Entity\Department;
+use App\Form\DepartmentFormType;
 class AdminController extends Controller {
-
+    public $terms;
+    public function admin() {
+        return new Response($this->render("Admin/dashboard.html.twig"));
+    }
     public function userInvite(Request $request, \Swift_Mailer $mailer) {
-
         $invite = new Invite();
-
-        # $inviteID = $inviteRepository->findBy();
-        # check email and id in database
         $form = $this->createForm(InviteFormType::class, $invite, ['standalone' => true]);
         $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $random = random_int(10000, 1999999);
-
-            $invite->setHash($random);
-
-            $email = $form->get("email")->getData();
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($invite);
-            $entityManager->flush();
-
-            $message = (new \Swift_Message('Hello Email'))
-                ->setFrom("support@inh.com")
-                ->setTo($email)
-                ->setBody(
-                    $this->renderView(
-                        'Email/invite.html.twig',
-                        ["invite" => $invite]
-                    ),
-                    'text/html'
-                )
-
-                ->addPart(
-                    $this->renderView(
-                        'Email/invite.txt.twig',
-                        ["invite" => $invite]
-                    ),
-                    'text/plain'
-                );
-
-            $mailer->send($message);
-
-
-            return $this->redirectToRoute('invite');
+        $emailList = $form->get("email")->getData();
+        $emails = explode(",", $emailList);
+        foreach($emails as $email){
+            if ($form->isSubmitted() && $form->isValid()) {
+                $random = random_int(10000, 1999999);
+                $invite = new Invite();
+                $invite->setHash($random);
+                $invite->setEmail($email);
+                #$email = $form->get("email")->getData();
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($invite);
+                $entityManager->flush();
+                $message = (new \Swift_Message('Hello Email'))
+                    ->setFrom("support@inh.com")
+                    ->setTo($email)
+                    ->setBody(
+                        $this->renderView(
+                            'Email/invite.html.twig',
+                            ["invite" => $invite]
+                        ),
+                        'text/html'
+                    )
+                    ->addPart(
+                        $this->renderView(
+                            'Email/invite.txt.twig',
+                            ["invite" => $invite]
+                        ),
+                        'text/plain'
+                    );
+                $mailer->send($message);
+                }
         }
-
         return new Response($this->renderView(
             'Admin/inviteForm.html.twig',
             ["inviteForm" => $form->createView()]
         ));
     }
-
     public function listInvites() {
-
         $inviteRepository = $this->getDoctrine()
             ->getManager()
             ->getRepository(Invite::class);
         $invites = $inviteRepository->findAll();
-
         return new Response($this->render("Admin/Lists/inviteList.html.twig", ["invites" => $invites]));
     }
-
     public function userList(Request $request) {
-
+        $roleChange = $request->get("change");
+        if(!isset($roleChange)) {
+            $roleChange = 2;
+        }
         
         $term = new UserSearch();
         $term2 = new UserSearch();
         $searchForm = $this->createForm(UserSearchFormType::class, $term, ['standalone' => true]);
-
         $searchForm->handleRequest($request);
         $terms = $term->getSearch();
         $termsSplit = explode(" ", $terms);
-        $found_users = "";
         if(isset($termsSplit[1])) {
             $term->setSearch($termsSplit[0]);
             $term2->setSearch($termsSplit[1]);
+            $lastSearchTerm = $term->getSearch() . " " . $term2->getSearch();
         }
         else {
             $term->setSearch($terms);
+            $lastSearchTerm = $term->getSearch();
         }
         
-    
         if (filter_var($terms, FILTER_VALIDATE_EMAIL)) {
             $users = $this->getDoctrine()->getManager()->getRepository(User::class)->findByEmail($term);
             
@@ -114,92 +102,59 @@ class AdminController extends Controller {
             $users = $userRepository->findAll(); 
         }
         
-        return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "searchForm" => $searchForm->createView(), "role" => false]));
+        return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "searchForm" => $searchForm->createView(), "role" => $roleChange]));
         
     }
-
-    public function makeAdmin(User $user, Request $request) {
-
-        $userRepository = $this->getDoctrine()
-            ->getManager()
-            ->getRepository(User::class);
-        $users = $userRepository->findAll();
-
-        $roleRepository = $this->getDoctrine()->getManager()->getRepository(Role::class);
-        $admin = $roleRepository->find(1);
-
-        if($user->getRoles() != $admin) {
-
-            $user->setRoles($admin);
+    public function departmentList(Request $request) {
+        $department = new Department();
+        $departmentForm = $this->createForm(DepartmentFormType::class, $department);
+        $departmentForm->handleRequest($request);
+        
+        $departments = $this->getDoctrine()->getManager()->getRepository(Department::class)->findAll();
+        if ($departmentForm->isSubmitted() && $departmentForm->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
+            $entityManager->persist($department);
             $entityManager->flush();
-
+        }
+        return new Response($this->render("Admin/Lists/departmentList.html.twig", ["departments" => $departments, "d_form" => $departmentForm->createView()]));
+    }
+    public function makeAdmin(User $user, Request $request) {
+        $roleRepository = $this->getDoctrine()->getManager()->getRepository(Role::class);
+        $role = $roleRepository->find(1);
+        if($user->getRoles()[0] != $role) {
+            $this->flushUser($user, $role);
             $email = $user->getEmail();
             $reason = "Email/Account/admin.html.twig";
             $this->sendMail($reason, $email);
-
-            return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => true]));
-        }        
-        
-        return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => false] ));
+            return $this->redirectToRoute("userList", ["change" => 0]);
+        }
+        return $this->redirectToRoute("userList", ["change" => 1]);
     }
-
-    public function removeAdmin(User $user, Request $request) {
-
-        $userRepository = $this->getDoctrine()
-            ->getManager()
-            ->getRepository(User::class);
-        $users = $userRepository->findAll();
-
+    public function makeUser(User $user, Request $request) {
         $roleRepository = $this->getDoctrine()->getManager()->getRepository(Role::class);
-        $normal = $roleRepository->find(2);
-
-        if ($user->getRoles() != $normal) {
-
-            $user->setRoles($normal);
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
+        $role = $roleRepository->find(2);
+        if ($user->getRoles()[0] != $role) {
+            $this->flushUser($user, $role);
             $email = $user->getEmail();
             $reason = "Email/Account/noAdmin.html.twig";
             $this->sendMail($reason, $email);
-
-            return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => true]));
+            return $this->redirectToRoute("userList", ["change" => 3]);
         }
+        return $this->redirectToRoute("userList", ["change" => 1]);
     }
-
-    public function deactivateUser(User $user, Request $request) {
-
-        $userRepository = $this->getDoctrine()
-            ->getManager()
-            ->getRepository(User::class);
-        $users = $userRepository->findAll();
-
+    public function makeInactive(User $user, Request $request) {
         $roleRepository = $this->getDoctrine()->getManager()->getRepository(Role::class);
         $inactive = $roleRepository->find(3);
-        $admin = $roleRepository->find(1);
-
-        if ($user->getRoles() != $admin && $user->getRoles() != $inactive ) {
-
-            $user->setRoles($inactive);
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-
+        $role = $roleRepository->find(1);
+        if ($user->getRoles()[0] != $role && $user->getRoles() != $inactive ) {
+            $this->flushUser($user, $inactive);
             $reason = "Email/Account/inactive.html.twig";
             $email = $user->getEmail();
             $this->sendMail($reason, $email);
-            return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => true]));
-        } 
-        else {
-            return new Response($this->render("Admin/Lists/userList.html.twig", ["users" => $users, "role" => "unable"]));
+            return $this->redirectToRoute("userList", ["change" => 4]);
         }
+        return $this->redirectToRoute("userList", ["change" => 1]);
     }
-
     public function sendMail(string $reason, string $email) {
         
         $transport = new \Swift_SmtpTransport("localhost:1025");
@@ -219,12 +174,13 @@ class AdminController extends Controller {
             ),
             'text/plain'
         );
-
         $mailer->send($message);
     }
-
-    public function search(string $term) {     
-        
+    public function flushUser(User $user, $role) {
+        $user->setRoles($role);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
     }
 
     public function userDetail(User $user, Request $request) {
@@ -237,17 +193,6 @@ class AdminController extends Controller {
         return new Response($this->render("Admin/Lists/userDetail.html.twig", ["user" => $user , "department" => $department ]));
     
     }
-
-    public function departmentDetail(Label $label, Icon $icon) {
-
-        $departmentId = $department->getId();
-        $manager = $this->getDoctrine()->getManager();
-        $manager->getRepository(Department::class)->findAll();
-
-        return new Response($this->render("Admin/Lists/departmentDetail.html.twig", ["department" => $department]));
-
-    }
-
 
 }
 
