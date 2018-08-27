b7a1bc8e1e7377e8b8bc216fe94f38622db57f38
sumuhire <steveumuhire@gmail.com>
2018-08-27 15:21:17 +0200

    Last changes

--- public/homebrew/questions.js
+++ public/homebrew/questions.js
@@ -23,6 +23,17 @@ $(function () {
             } 
         });
     });
+
+    $.get("/get/myQuestions").done(function (res) {
+        question = res;
+        for(let i = 0; i < question.length; i++)  {
+            let title = question[i].title;
+            let newTitle = title.substring(0, 100);
+            $('#myQuestions').append('<div class="card card-task" style="height:10rem"><div class="progress"><div class="progress-bar bg-warning" role="progressbar" style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div></div><div class="card-body"><div class="card-title"><a href="question/' + question[i].id + '"><h6 data-filter-by="text" style="font-size:20px">' + newTitle + '</h6></a><p class="card-text"><small class="text-muted" style="font-size:12px">Last updated ' + question[i].creationDate + '</small></p></div><div class="dropdown card-options"><div class="card-meta" style="margin-top:3px;margin-bottom:5px"><a href="/profile/visit/' + question[i].user.username + '" data-toggle="tooltip" title=""><img alt="Kenny Tran" class="avatar" style="" src="uploads/picture/' + question[i].user.picture + '" /> <p> ' + question[i].user.username + '</p> </a> </div><div class="card-meta" style="font-size:13px"><br /><p>' + question[i].targetDepartment.label + '</p></div></div></div></div >');
+        } 
+    });
+
+
     $('#AAA-tab').bind('click', function() {
         $.get("/get/departmentQuestions").done(function (res) {
             question = res;
